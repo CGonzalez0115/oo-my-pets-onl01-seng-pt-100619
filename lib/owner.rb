bfbb124b880047fa7a0f6f209e5ec4c4f08c9fe8
Christina Gonzalez <cmgamway@gmail.com>
2019-11-11 01:13:33 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,4 +1,5 @@
 class Owner
+<<<<<<< HEAD
   attr_accessor :pets
   attr_reader :name, :species
   @@all = []
@@ -8,3 +9,8 @@ class Owner
     @@all << self
   end
 end
+=======
+  attr_accessor :name, :pets
+  attr_reader :species
+end
+>>>>>>> 6d8fbce53054f40e09c18759e22e7d79df47f21f
